df83112c7426502d0e90bbd4dbd9d6fb467e58af
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-21 20:24:34 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -90,7 +90,7 @@ end
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
   # return the entire updated hash
-
+alans_new_info = "GUI"
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -98,7 +98,7 @@ def changing_alan
         :languages => ["COBOL", "FORTRAN"]
       },
       :alan_kay => {
-        :known_for => "Object Orientation",
+        :known_for => alans_new_info,
         :languages => ["Smalltalk", "LISP"]
       },
       :dennis_ritchie => {
