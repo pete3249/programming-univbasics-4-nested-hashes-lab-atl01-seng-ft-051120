f5c7ef91da5a5635da039c45d23c625f49bfc961
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-21 20:09:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -66,14 +66,8 @@ def adding_matz
 # }
 # return the entire updated hash
 
-
-
-
-
-
-
-
-	programmer_hash = {
+programmer_hash = 
+    {
 	    :yukihiro_matsumoto => {
 	      :known_for => "Ruby",
 	      :languages => ["LISP", "C"]
